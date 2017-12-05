22a5f3481bed5ddf0a8c8f56adae2518a1e3f79e
lindermanurrego <lindermanu@gmail.com>
2017-12-05 16:19:13 -0500

    commit

--- app/src/main/java/com/example/usre/pokedex/PokemonListAdanpter.java
+++ app/src/main/java/com/example/usre/pokedex/PokemonListAdanpter.java
@@ -2,7 +2,12 @@ package com.example.usre.pokedex;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,5 +27,24 @@ public class PokemonListAdanpter extends ArrayAdapter<Pokemon>{
         this.context = context;
         this.pokemonList = pokemonList;
         layoutId = resource;
+    }
+
+    public View getView (int position, View convertView, ViewGroup parent){
+
+        PokemonViewHolder holder;
+
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        }
+
+
+    }
+
+
+    private  class PokemonViewHolder{
+        public TextView pokemonIdTextView;
+        public TextView pokemonNameTextView;
+        public ImageView pokemonTypeImageView;
     }
 }
