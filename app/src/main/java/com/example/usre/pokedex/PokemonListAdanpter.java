7766804e74d4919aadb4917e6082703f5daf4dcd
lindermanurrego <lindermanu@gmail.com>
2017-12-05 16:32:21 -0500

    commit

--- app/src/main/java/com/example/usre/pokedex/PokemonListAdanpter.java
+++ app/src/main/java/com/example/usre/pokedex/PokemonListAdanpter.java
@@ -2,6 +2,7 @@ package com.example.usre.pokedex;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +17,7 @@ import java.util.List;
  * Created by Usre on 3/12/2017.
  */
 
-public class PokemonListAdanpter extends ArrayAdapter<Pokemon>{
+public class PokemonListAdapter extends ArrayAdapter<Pokemon>{
      private Context context;
      private ArrayList<Pokemon> pokemonList;
      private int layoutId;
@@ -36,9 +37,40 @@ public class PokemonListAdanpter extends ArrayAdapter<Pokemon>{
         if (convertView == null){
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
+            convertView = inflater.inflate(layoutId,null);
+
+            holder = new PokemonViewHolder();
+
+            holder.pokemonIdTextView = convertView.findViewById(R.id.pokemon_list_item_id_textView);
+            holder.pokemonNameTextView = convertView.findViewById(R.id.pokemon_list_item_name_textView);
+            holder.pokemonTypeImageView = convertView.findViewById(R.id.pokemon_list_item_type_image);
+            convertView.setTag(holder);
+
+        }else{
+            holder = (PokemonViewHolder) convertView.getTag();
         }
 
+        Pokemon pokemon = pokemonList.get(position);
+        holder.pokemonIdTextView.setText(String.valueOf(pokemon.getId()));
+        holder.pokemonNameTextView.setText(pokemon.getName());
 
+        final Pokemon.Type type = pokemon.getType();
+
+        switch (type){
+            case FIRE:
+                holder.pokemonTypeImageView.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.fire));
+                break;;
+            case WATER:;
+                holder.pokemonTypeImageView.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.water));
+                break;;
+            case PLANT:;
+                holder.pokemonTypeImageView.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.plant));
+                break;;
+            case ELECTRIC:;
+                holder.pokemonTypeImageView.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.electric));
+                break;;
+        }
+        return  convertView;
     }
 
 
